f2346c8868734ed0ba25cc50c7c99b455557b892
Vanshika Rajput <vanshikavce19@gmail.com>
2025-02-11 20:38:37 +0530

    fix: update chat API endpoint to new production URL

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -21,7 +21,7 @@ import Lottie from "react-lottie";
 import "./style.css";
 import animationData from "../animations/typing.json";
 
-const ENDPOINT = "https://talk-a-tive-tnze.onrender.com/";
+const ENDPOINT = "https://mern-chat-app-ww9s.onrender.com/";
 var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
